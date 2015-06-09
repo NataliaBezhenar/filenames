f53710e130333ef9d2e5a56f888dcb22d93dee56
NataliaBezhenar <natalia.bezhenar@gmail.com>
2015-06-09 17:23:48 +0300

    1. modified method generateUniqueName 2. .gitignore extended version

--- src/main/java/hrytsenko/Filenames.java
+++ src/main/java/hrytsenko/Filenames.java
@@ -1,6 +1,9 @@
 package hrytsenko;
 
+
 import java.util.List;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 
 /**
  * Utilities for working with filenames.
@@ -23,7 +26,28 @@ public final class Filenames {
      * @return the generated unique name.
      */
     public static String generateUniqueName(String originalName, List<String> knownNames) {
-        throw new UnsupportedOperationException("Not implemented.");
+        //throw new UnsupportedOperationException("Not implemented.");
+    	String extention = FilenameUtils.getExtension(originalName);
+        String filename; 
+      
+        String baseName = FilenameUtils.getBaseName(originalName);
+
+
+
+        if (knownNames.contains(originalName))
+         {
+        	String name =String.format("%s.%s", RandomStringUtils.randomNumeric(3),extention);
+        	 filename =  baseName+name;
+        	
+        }
+       
+        else filename=originalName;
+     
+        
+       return filename;
+     
+      
+    			
     }
 
     private Filenames() {
